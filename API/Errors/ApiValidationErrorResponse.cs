f82198a81b9e0d5f7f7a6a114e587abaf2041186
Nitesh <niteshmishra001@gmail.com>
2024-05-15 22:18:53 +0100

    Error Handling and Swagger

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse( string message = null) : base(400)
+        {
+
+        }
+        public IEnumerable<string> Errors { get; set; }
+
+    }
+}
