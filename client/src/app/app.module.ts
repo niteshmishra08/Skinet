a2bccf728e61b350846e5a2f7152504059c125f5
Nitesh <niteshmishra001@gmail.com>
2024-05-26 11:45:45 +0100

    Client Error Handling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,6 +9,7 @@ import { ShopComponent } from './shop/shop.component';
 import { ShopModule } from './shop/shop.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,10 @@ import { HomeModule } from './home/home.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi: true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
