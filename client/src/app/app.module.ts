419b0b37c261816385edbe8436213bb1bd94a247
Nitesh <niteshmishra001@gmail.com>
2024-05-26 20:41:47 +0100

    Client Enhancements

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ShopModule } from './shop/shop.module';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
     HomeModule
   ],
   providers: [
-{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi: true}
+{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi: true},
+{provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor,multi: true},
 
   ],
   bootstrap: [AppComponent]
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { CarouselComponent } from 'ngx-bootstrap/carousel';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent
+
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomeComponent
